a3b6a10013ec5fd4b49e5665d0fed871735a463a
Abeldenius <zhazleton@gmail.com>
2023-10-19 19:17:31 +0200

    Adding middleware

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/receive_texts/")
+async def receive_texts(texts: List[str]):
+    for text in texts:
+        print(f"Received text: {text}")
+    return {"message": "Texts received"}
