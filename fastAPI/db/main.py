ac2e5f022d162c3ad881fc47f92c4df7f68f7266
Abeldenius <zhazleton@gmail.com>
2023-10-20 14:47:25 +0200

    Insertion of new entry

--- fastAPI/db/main.py
+++ fastAPI/db/main.py
@@ -6,7 +6,6 @@ import pandas as pd
 column_data = ["""Sagsnummer", "Enhed", "Mærke", "Model", "Serienummer", "ID", "Modtaget af", 
                "Ansvarlig", "Noter", "Tid og dato", "Mail", "Password", "Lokation"""]
 overview = pd.DataFrame(columns=column_data)
-
 con = sqlite3.connect("psd.db")
 cur = con.cursor()
 cur.execute(""" CREATE TABLE IF NOT EXISTS
@@ -15,6 +14,23 @@ cur.execute(""" CREATE TABLE IF NOT EXISTS
             model, serial_number , ID, 
             received, responsible, notes, 
             time, mail, password, storage_location)""")
+
+
+def insertion(values: dict):
+    cur.execute("""INSERT INTO Data(
+                case_number, device, manufacturer,
+                model, serial_number , ID, 
+                received, responsible, notes, 
+                time, mail, password, storage_location)
+                 VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)""", 
+                (values["Sagsnummer"], values["Mærke"], values["Enhed"], 
+                values["Model"], values["Serienummer"], values["ID"], 
+                values["Afsender"], values["Modtager"], values["Eventuelle noter"], 
+                values["Tid og dato"], values["Email"], values["Password"], 
+                values["Lokation for opbevaring"]))
+    con.commit()
+
+
 
 # con.commit()
 
