e0ad5c04646029efd90e31f8bfe6dd18e4c315aa
Abeldenius <zhazleton@gmail.com>
2023-10-20 15:48:07 +0200

    Adding new column (size)

--- fastAPI/db/main.py
+++ fastAPI/db/main.py
@@ -1,33 +1,30 @@
 import sqlite3
 import pandas as pd 
-
+from db.setup import db_start
 # Connecting
 
+db_start()
+
 column_data = ["""Sagsnummer", "Enhed", "Mærke", "Model", "Serienummer", "ID", "Modtaget af", 
-               "Ansvarlig", "Noter", "Tid og dato", "Mail", "Password", "Lokation"""]
+               "Ansvarlig", "Noter", "Tid og dato", "Mail", "Password", "Størrelse/brugt" "Lokation"""]
 overview = pd.DataFrame(columns=column_data)
-con = sqlite3.connect("psd.db")
-cur = con.cursor()
-cur.execute(""" CREATE TABLE IF NOT EXISTS
-            Data(
-            case_number, device, manufacturer,
-            model, serial_number , ID, 
-            received, responsible, notes, 
-            time, mail, password, storage_location)""")
+
 
 
 def insertion(values: dict):
+    con = sqlite3.connect("psd.db")
+    cur = con.cursor()
     cur.execute("""INSERT INTO Data(
                 case_number, device, manufacturer,
                 model, serial_number , ID, 
                 received, responsible, notes, 
-                time, mail, password, storage_location)
-                 VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)""", 
+                time, mail, password, size, storage_location)
+                 VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)""", 
                 (values["Sagsnummer"], values["Mærke"], values["Enhed"], 
                 values["Model"], values["Serienummer"], values["ID"], 
                 values["Afsender"], values["Modtager"], values["Eventuelle noter"], 
                 values["Tid og dato"], values["Email"], values["Password"], 
-                values["Lokation for opbevaring"]))
+                values["Størrelse"], values["Lokation for opbevaring"]))
     con.commit()
 
 
