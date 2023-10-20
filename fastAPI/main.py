b928cdc206740ec66362140608f37343be9e44df
Abeldenius <zhazleton@gmail.com>
2023-10-20 15:48:22 +0200

    Handlind new insertion and key values

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from typing import Dict
 from fastapi.middleware.cors import CORSMiddleware
+from db.id_generator import generate_id
+from datetime import datetime
+from db.main import insertion
+
 
 
 app = FastAPI()
@@ -16,6 +20,10 @@ app.add_middleware(
 
 @app.post("/receive_texts/")
 async def receive_texts(texts: Dict[str, str]):
-    for key, value in texts.items():
-        print(f"{key}: {value}")
+    for key, values in texts.items():
+        print(key, values)
+    texts['ID'] = generate_id(texts["Enhed"], texts["Sagsnummer"])
+    texts["Tid og dato"] = datetime.now().strftime("%d-%m-%Y %H:%M")
+    insertion(texts)
+
     return {"message": "Data received"}
