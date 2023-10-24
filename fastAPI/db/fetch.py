ddedddb612dcdc628142c495597a52d9fb856aa8
Abeldenius <zhazleton@gmail.com>
2023-10-24 15:55:16 +0200

    Changing output format for fetch.py

--- fastAPI/db/fetch.py
+++ fastAPI/db/fetch.py
@@ -6,8 +6,38 @@ def fetch_data():
     cur = con.cursor()
     cur.execute("""SELECT * FROM Data""")
     data = cur.fetchall()
-    for x in data:
-        print(x)
+    
+    # Get the column names from the cursor description
+    column_names = [
+        "Sagsnummer",
+        "Enhed",
+        "Mærke",
+        "Model",
+        "Serienummer",
+        "ID",
+        "Afsender",
+        "Modtager",
+        "Noter",
+        "Tid",
+        "Mail",
+        "Password",
+        "Størrelse",
+        "Lokation"
+    ]
+    
+    # Define a list to store the data as dictionaries
+    formatted_data = []
+    
+    for row in data:
+        # Create a dictionary for each row by zipping column names and row values
+        data_dict = dict(zip(column_names, row))
+        
+        # Append the dictionary to the list
+        formatted_data.append(data_dict)
+    
+    return formatted_data
 
-
-fetch_data()
+# Fetch and print the formatted data
+formatted_data = fetch_data()
+for item in formatted_data:
+    print(item)
